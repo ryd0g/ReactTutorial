ff87f7e90719ca6075b8c56a54e211ad562184f7
Ryan Lee <rjlguitar@gmail.com>
2020-11-16 18:25:28 -0800

    added dynamic data for each location

--- sfpopos/src/POPOSList.js
+++ sfpopos/src/POPOSList.js
@@ -1,42 +1,21 @@
 import React from 'react';
 import POPOSSpace from './POPOSSpace';
 import './POPOSList.css';
+import data from './sfpopos-data.json';
 
 function POPOSList() {
-  return (
-    <div className='POPOSList'>
+  const spaces = data.map(({ title, address, images, hours }) => {
+    return (
       <POPOSSpace
-        name='Garden Terrace'
-        address='150 California St.'
-        image='garden-terrace-at-150-california.jpg'
+        name={title}
+        address={address}
+        image={images[0]}
+        hours={hours} // add a new prop for hours here
       />
-      <POPOSSpace
-        name='100 Pine Street'
-        address='100 Pine St.'
-        image='100-pine.jpg'
-      />
-      <POPOSSpace
-        name='Citigroup Center'
-        address='1 Sansome St.'
-        image='citigroup-center.jpg'
-      />
-      <POPOSSpace
-        name='Embarcadero Center'
-        address='San Francisco, CA 94111'
-        image='embarcadero-center.jpg'
-      />
-      <POPOSSpace
-        name='Sansome Roof Garden'
-        address='343 Sansome St.'
-        image='343-sansome-roof-garden.jpg'
-      />
-      <POPOSSpace
-        name='Market Street Plaza'
-        address='525 Market St.'
-        image='525-market-street-plaza.jpg'
-      />
-    </div>
-  );
+    );
+  });
+
+  return <div className='POPOSList'>{spaces}</div>;
 }
 
 export default POPOSList;
