099a852dedfd0ad5c107400ae01a5149fc65cefa
Ryan Lee <rjlguitar@gmail.com>
2020-11-16 16:55:44 -0800

    styled the images in grid, fixed images/names

--- sfpopos/src/POPOSList.js
+++ sfpopos/src/POPOSList.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import POPOSSpace from './POPOSSpace';
+import './POPOSList.css';
 
 function POPOSList() {
   return (
     <div className='POPOSList'>
       <POPOSSpace
-        name='50 California Street'
-        address='50 California St.'
-        image='50-california-st.jpg'
+        name='Garden Terrace'
+        address='150 California St.'
+        image='garden-terrace-at-150-california.jpg'
       />
       <POPOSSpace
         name='100 Pine Street'
@@ -15,9 +16,9 @@ function POPOSList() {
         image='100-pine.jpg'
       />
       <POPOSSpace
-        name='101 California Street'
-        address='101 California St.'
-        image='101-california.jpg'
+        name='Citigroup Center'
+        address='1 Sansome St.'
+        image='citigroup-center.jpg'
       />
       <POPOSSpace
         name='Embarcadero Center'
@@ -25,12 +26,12 @@ function POPOSList() {
         image='embarcadero-center.jpg'
       />
       <POPOSSpace
-        name='343 Sansome Roof Garden'
+        name='Sansome Roof Garden'
         address='343 Sansome St.'
         image='343-sansome-roof-garden.jpg'
       />
       <POPOSSpace
-        name='525 Market Street Plaza'
+        name='Market Street Plaza'
         address='525 Market St.'
         image='525-market-street-plaza.jpg'
       />
