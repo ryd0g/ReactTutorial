9c03da96886f1e7f80769cc14b27c53d21598848
Ryan Lee <rjlguitar@gmail.com>
2020-11-16 17:15:41 -0800

    added footer

---
+++ sfpopos/src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className='Footer'>
+      <p>Ryan Lee © 2020</p>
+    </footer>
+  );
+}
+
+export default Footer;
