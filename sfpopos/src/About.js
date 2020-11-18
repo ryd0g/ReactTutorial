78cbf520cda551a155d216f34e49dbd221d610ec
Ryan Lee <rjlguitar@gmail.com>
2020-11-18 10:56:56 -0800

    fixed font family on h1

--- sfpopos/src/About.js
+++ sfpopos/src/About.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './About.css';
 
 function About() {
   return (
-    <div>
+    <div className='About'>
       <h1>About SFPOPOS</h1>
       <p>
         POPOS are publicly accessible spaces in forms of plazas, terraces,
