569254c93c35fd1e648032b41344ade7c4178732
Ryan Lee <rjlguitar@gmail.com>
2020-11-18 15:59:14 -0800

    fixed data file causing compiling error

--- sfpopos/src/components/POPOSDetails.js
+++ sfpopos/src/components/POPOSDetails.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 
-import data from '../sfpopos-data.json';
+import data from './sfpopos-data.json';
 
 function POPOSDetails(props) {
   const { id } = props.match.params; // Location index
