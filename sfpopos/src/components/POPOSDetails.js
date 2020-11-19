207fb3d2a15cd220627666e41e8d6957aca75744
Ryan Lee <rjlguitar@gmail.com>
2020-11-18 16:50:01 -0800

    added emoji to details list

--- sfpopos/src/components/POPOSDetails.js
+++ sfpopos/src/components/POPOSDetails.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import './POPOSDetails.css';
 import data from './sfpopos-data.json';
+import POPOSFeatureList from './POPOSFeatureList';
 
 function POPOSDetails(props) {
   const { id } = props.match.params; // Location index
@@ -17,7 +18,7 @@ function POPOSDetails(props) {
         <h1 className='title'>{title}</h1>
         <p className='desc'>{desc}</p>
         <p className='hours'>{hours}</p>
-        <p className='features'>{features}</p>
+        <POPOSFeatureList features={features} />
         <p className='geo'>
           {geo.lat} {geo.lon}
         </p>
